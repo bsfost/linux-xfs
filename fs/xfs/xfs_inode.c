1577b5bc0d33f29b4507db3eb6bc5187bdc13977
Brian Foster <bfoster@redhat.com>
2020-04-15 09:45:08 -0400

    xfs: factor out buffer I/O failure simulation code
    We use the same buffer I/O failure simulation code in a few
    different places. It's not much code, but it's not necessarily
    self-explanatory. Factor it into a helper and document it in one
    place.
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    Reviewed-by: Allison Collins <allison.henderson@oracle.com>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -3629,12 +3629,7 @@ cluster_corrupt_out:
 	 * xfs_buf_submit().
 	 */
 	ASSERT(bp->b_iodone);
-	bp->b_flags |= XBF_ASYNC;
-	bp->b_flags &= ~XBF_DONE;
-	xfs_buf_stale(bp);
-	xfs_buf_ioerror(bp, -EIO);
-	xfs_buf_ioend(bp);
-
+	xfs_buf_ioend_fail(bp, XBF_ASYNC);
 	xfs_force_shutdown(mp, SHUTDOWN_CORRUPT_INCORE);
 
 	/* abort the corrupt inode, as it was not attached to the buffer */
